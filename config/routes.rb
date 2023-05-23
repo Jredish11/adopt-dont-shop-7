7bfb07d942375294f78ba447a5af0cfd3a97f079
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-05-23 08:42:57 -0600

    fix route

--- config/routes.rb
+++ config/routes.rb
@@ -42,7 +42,7 @@ Rails.application.routes.draw do
   get "/applications/:id", to: "applications#show"
   patch "/applications/:id", to: "applications#update"
 
-  get "/admin/applications/:id", to: "admin/applications#show"
-
   get "/admin/shelters", to: "admin#shelters_index"
+  get "/admin/applications/:id", to: "admin#show"
+  patch "/admin/applications/:id", to: "admin#update"
 end
